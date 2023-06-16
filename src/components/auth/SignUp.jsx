2ce03d4817de15224b5b4bfa61222ccfa0e2586c
0xDV <di593297724ma@gmail.com>
2023-06-16 23:37:44 +0200

    update style sign up

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -7,6 +7,7 @@ const SingUp = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+
     const singUp = (e) => {
         e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
@@ -17,31 +18,37 @@ const SingUp = () => {
         })
     }
 
-    // useEffect(() => {
-    //     console.log(email  password);
-    // }, [email, password])
+    return (
+        <div className='sing-up-container'>
+            <div className='sing-up-field'>
+                <img src="./images/btc-bg.png" id='btc-bg' alt="" />
+                <img src="./images/tz-bg.png" id='tz-bg' alt="" />
+                <img src="./images/doge-bg.png" id='doge-bg' alt="" />
+                <form className='sign-up-content' action="" onSubmit={singUp}>
+                    <h1 className='sing-up-title'>Create Account</h1>
 
-  return (
-    <div className='sing-up-container'>
-        <div className='sing-up-field'>
-            <img src="./images/btc-bg.png" id='btc-bg' alt="" />
-            <img src="./images/tz-bg.png" id='tz-bg' alt="" />
-            <img src="./images/doge-bg.png" id='doge-bg' alt="" />
-            <form className='sign-up-content' action="" onSubmit={singUp}>
-                <h1 className='sing-up-title'>Create Account</h1>
-                
-                <input className="input-field" type="email" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <input className="input-field" type="password" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <input className="input-field" type="password" placeholder='Confirm password'/>
-                <button className='submit-button' type='submit'>Sign Up</button>
-            </form>
+                    <div className='inputBox'>
+                        <input className="input-field" type="email"  value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                        <label htmlFor="">Email</label>
+                    </div>
+                    <div className='inputBox'>
+                        <input className="input-field" type="password"  value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                        <label htmlFor="">Password</label>
+                    </div>
+                    <div className='inputBox'>
+                        <input className="input-field" type="password" required/>
+                        <label htmlFor="">Confirm Password</label>
+                    </div>
+
+                    <button className='submit-button' type='submit'>Sign Up</button>
+                </form>
+            </div>
+            <div className='login-content'>
+                <p className='login-text'>Already have an account?</p>
+                <a href="">Log In</a>
+            </div>
         </div>
-        <div className='login-content'>
-            <p className='login-text'>Already have an account?</p>
-            <a href="">Log In</a>
-        </div>
-    </div>
-  )
+    )
 }
 
-export default SingUp
+export default SingUp;
