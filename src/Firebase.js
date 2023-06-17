cfdcb8bcb78ef3b255507978d365ce329c4ef93b
0xDV <di593297724ma@gmail.com>
2023-06-17 22:16:19 +0200

    cryptocurrencies update realtime

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +15,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+const database = getDatabase(app);
+
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export default database;
