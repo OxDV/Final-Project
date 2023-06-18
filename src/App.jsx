52d40a25ef32297625d4ad43dba39ec1ba216ef4
0xDV <di593297724ma@gmail.com>
2023-06-18 14:11:51 +0200

    work with 2function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ref, onValue } from 'firebase/database';
 import database from './firebase'; // Import the 'database' object
 
@@ -13,30 +13,45 @@ import Register from './pages/Register';
 import Account from './pages/Account';
 
 function App() {
+  const [data, setData] = useState({});
+  const [coinData, setCoinData] = useState('');
+  const [lastUpdate, setLastUpdate] = useState('');
+
   useEffect(() => {
     const fetchData = () => {
       const databaseRef = ref(database); // Reference to the root of your database
       onValue(databaseRef, (snapshot) => {
         const data = snapshot.val();
-        console.log(data); // Log the fetched data in the console
+        setData(data);
       });
     };
 
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (data.coinData && data.coinData.length > 0) {
+      setCoinData(data.coinData);
+      console.log(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    console.log(coinData);
+  }, [coinData]);
+
   return (
     <BrowserRouter>
-      <div className='app'>
+      <div className="app">
         <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/why_us' element={<WhyUs />} />
-          <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
-          <Route path='/contacts' element={<Contacts />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/account' element={<Account />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/why_us" element={<WhyUs />} />
+          <Route path="/cryptocurrencies" element={<Cryptocurrencies data={coinData} />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </div>
     </BrowserRouter>
