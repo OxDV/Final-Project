10cb29cd07586a007f26ab46f8718ae6e61b666a
Dmytro Vyshnevskyi <di593297724ma@gmail.com>
2023-06-19 21:42:41 +0200

    make page why us

--- src/pages/WhyUs.jsx
+++ src/pages/WhyUs.jsx
@@ -1,12 +1,83 @@
-import React from 'react'
-import "../styles/pages/whyUs.scss"
+import React from "react";
+import "../styles/pages/whyUs.scss";
 
 export default function WhyUs() {
   return (
-    <div className='why-us-container'>
-      <div className='why-us-content'>
-      <h1 className='why-us-tittel'>We are Your Best Choise</h1>
+    <div className="why-us-container">
+      <div className="why-us-content">
+        <h1 className="why-us-tittel">We are Your Best Choise</h1>
+
+        <div className="why-us-info">
+          <div className="why-us-advantages">
+            <div>
+              <h3>LOW COMMISSIONS</h3>
+              <p>
+                Maximize Your Crypto Purchases with Our Lowest Possible Fees
+              </p>
+            </div>
+            <div>
+              <h3>FAST EXCHANGE</h3>
+              <p>
+                Receive your crypto quickly, after you complete the payment.
+              </p>
+            </div>
+            <div>
+              <h3>WARRANTY</h3>
+              <p>
+                100% guarantee. The reliability of our service is confirmed by
+                numerous reviews.
+              </p>
+            </div>
+            <div>
+              <h3>SUPPORT</h3>
+              <p>
+                Polite technical support around the clock ready to help you on
+                any matter.
+              </p>
+            </div>
+          </div>
+          <div className="why-us-images">
+            <img className="circle-bg" src="./images/circle-bg.svg" alt="" />
+
+            <div className="image-container-first">
+              <div className="square-first">
+                <img src="./images/square-bg.png" alt="" />
+                <div className="text-overlay-first">
+                  <img src="./images/low-commissions.png" alt="" />
+                  <h3>LOW COMMISSIONS</h3>
+                </div>
+              </div>
+            </div>
+            <div className="image-container-second">
+              <div className="square-second">
+                <img src="./images/square-bg.png" alt="" />
+                <div className="text-overlay-second">
+                  <img src="./images/low-commissions.png" alt="" />
+                  <h3>LOW COMMISSIONS</h3>
+                </div>
+              </div>
+            </div>
+            <div className="image-container-third">
+              <div className="square-third">
+                <img src="./images/square-bg.png" alt="" />
+                <div className="text-overlay-third">
+                  <img src="./images/low-commissions.png" alt="" />
+                  <h3>LOW COMMISSIONS</h3>
+                </div>
+              </div>
+            </div>
+            <div className="image-container-four">
+              <div className="square-four">
+                <img src="./images/square-bg.png" alt="" />
+                <div className="text-overlay-four">
+                  <img src="./images/low-commissions.png" alt="" />
+                  <h3>LOW COMMISSIONS</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
